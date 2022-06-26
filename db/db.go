9ce287d3f78cf2ffbb8b8d3e35b36d8452f20cfb
Simon <dingchuan91@gmail.com>
2022-06-26 10:33:04 +0800

    update

--- db/db.go
+++ db/db.go
@@ -71,10 +71,10 @@ func Open(dir string) {
 	newLogger := logger.New(
 		log2.New(os.Stdout, "\r\n", log2.LstdFlags), // io writer
 		logger.Config{
-			SlowThreshold:             time.Second,   // Slow SQL threshold
-			LogLevel:                  logger.Silent, // Log level
-			IgnoreRecordNotFoundError: true,          // Ignore ErrRecordNotFound error for logger
-			Colorful:                  false,         // Disable color
+			SlowThreshold:             time.Second, // Slow SQL threshold
+			LogLevel:                  logger.Info, // Log level
+			IgnoreRecordNotFoundError: true,        // Ignore ErrRecordNotFound error for logger
+			Colorful:                  false,       // Disable color
 		},
 	)
 
